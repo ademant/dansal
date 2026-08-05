db149cd403882d4a4ff6705c4e7f29b9c20e1735
mithunjoby <mithunjoby@gmail.com>
2026-08-05 23:32:50 +0530

    Fix tag href in ActivityPub objects to point to /tags/{slug}
    Resolves #954

--- cmd/dansal_web/actor.go
+++ cmd/dansal_web/actor.go
@@ -937,7 +937,7 @@ func buildAPEvent(cfg *Config, slug string, e Event) APEvent {
 		apEvent.Tag = append(apEvent.Tag, APHashtag{
 			Type: "Hashtag",
 			Name: "#" + tag,
-			Href: fmt.Sprintf("https://%s/?tag=%s", cfg.Domain, tag),
+			Href: fmt.Sprintf("https://%s/tags/%s", cfg.Domain, tag),
 		})
 	}
 	if e.ImageURL != "" {
@@ -1040,7 +1040,7 @@ func buildNoteFromEvent(cfg *Config, slug string, e Event) APNote {
 		note.Tag = append(note.Tag, APHashtag{
 			Type: "Hashtag",
 			Name: "#" + tag,
-			Href: fmt.Sprintf("https://%s/?tag=%s", cfg.Domain, tag),
+			Href: fmt.Sprintf("https://%s/tags/%s", cfg.Domain, tag),
 		})
 	}
 	if e.ImageURL != "" {
